febe4fc192d0e8c0da96504def6f54a3ac9833f7
JimmyJambino <jimmy_losang@hotmail.com>
2021-11-29 14:12:28 +0100

    fff

--- src/main/resources/static/makeCoords.js
+++
@@ -1,8 +0,0 @@
-function showPosition(position) {
-    document.getElementById("coords").innerText = "("+position.coords.latitude+","+position.coords.altitude+")"
-
-}
-
-function getCoords() {
-    navigator.geolocation.getCurrentPosition(showPosition)
-}
